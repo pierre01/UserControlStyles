861c646fd15b344348a61c228ceb2fd682e71125
Pierre Huguet <pierre@delange.design>
2025-07-14 21:58:26 -0700

    Add project files.

---
+++ UserControlStyles/UserControls/MyCustomControl.xaml.cs
@@ -0,0 +1,39 @@
+namespace UserControlStyles.UserControls;
+
+public partial class MyCustomControl : ContentView
+{
+    public static readonly BindableProperty UpperTextProperty =
+	    BindableProperty.Create(nameof(UpperText), typeof(string), typeof(MyCustomControl), "Up", propertyChanged: OnUpperTextChanged);
+
+    public static readonly BindableProperty UpperRectColorProperty = 
+        BindableProperty.Create(nameof(UpperRectColor), typeof(Brush), typeof(MyCustomControl), new SolidColorBrush(Colors.AliceBlue), propertyChanged: OnUpperRectColorChanged);
+
+    public MyCustomControl()
+	{
+		InitializeComponent();
+        UpTextLabel.Text = UpperText;
+        UpRectangle.Fill = UpperRectColor;
+    }
+
+    private static void OnUpperTextChanged(BindableObject d, object oldValue, object newValue)
+    {
+    }
+
+    private static void OnUpperRectColorChanged(BindableObject d, object oldValue, object newValue)
+    {
+    }
+
+    public string UpperText
+    {
+        get { return (string)GetValue(UpperTextProperty); }
+        set { SetValue(UpperTextProperty, value); }
+    }
+
+    public Brush UpperRectColor
+    {
+        get { return (Brush)GetValue(UpperRectColorProperty); }
+        set { SetValue(UpperRectColorProperty, value); }
+    }
+
+
+}
